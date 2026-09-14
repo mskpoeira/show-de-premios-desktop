aa459532641a2a9e9a394b06e690040eadebc4cb
mskpoeira <65519524+mskpoeira@users.noreply.github.com>
2026-09-13 23:06:04 -0300

    fix: expõe fluxo de autenticação seguro no preload

--- src/preload.js
+++ src/preload.js
@@ -1,8 +1,12 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('showApp', {
+  authStatus: () => ipcRenderer.invoke('auth:status'),
+  setupMaster: password => ipcRenderer.invoke('auth:setup-master', password),
+  login: (login, password) => ipcRenderer.invoke('auth:login', login, password),
+  logout: () => ipcRenderer.invoke('auth:logout'),
   load: () => ipcRenderer.invoke('data:load'),
   save: data => ipcRenderer.invoke('data:save', data),
-  exportBackup: data => ipcRenderer.invoke('data:export', data),
+  exportBackup: () => ipcRenderer.invoke('data:export'),
   importBackup: () => ipcRenderer.invoke('data:import'),
   openScreen: () => ipcRenderer.invoke('screen:open'),
   closeScreen: () => ipcRenderer.invoke('screen:close'),
